89251eb0c1a9b5d872f0ca5aa9bdc67866585c81
xupp <ping2428572>
2021-02-23 11:16:42 +0800

    删除没有用的代码

---
+++ springboot-kafka-monitior/src/test/java/com/xpp/springbootkafkamonitior/SpringbootKafkaMonitiorApplicationTests.java
@@ -0,0 +1,11 @@
+package com.xpp.springbootkafkamonitior;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaMonitiorApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
